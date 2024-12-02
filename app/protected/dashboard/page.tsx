54633a99954f41d89af9a755ebc805e19dc9a0fe
abhishek-shivale <abhishekshivale45@gmail.com>
2024-12-02 15:30:08 +0530

    re-initilzing repo

--- app/protected/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function page() {
-  return <div className='flex justify-center items-center h-screen w-full'>
-    <> YOU ARE LOGGED IN, YOU CAN ACCESS THIS PAGE</>
-  </div>;
-}
-
-export default page;
--- components/spinner/Loader.tsx
+++
@@ -1,8 +0,0 @@
-import { Loader2 } from 'lucide-react';
-import './Loader.css';
-
-function Loader({ color }: { color?: string }) {
-  return <Loader2 color={color} className='spinner inline-block text-white' />;
-}
-
-export default Loader;
